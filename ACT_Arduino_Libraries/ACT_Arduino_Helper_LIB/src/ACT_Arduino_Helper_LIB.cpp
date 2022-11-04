022b825e1dcd48e628188763fe76782c909a7af8
Mark Alicz <mark@inspiredvoltage.com>
2022-11-04 04:53:01 -0500

    aaa
    aaa

---
+++ ACT_Arduino_Libraries/ACT_Arduino_Helper_LIB/src/ACT_Arduino_Helper_LIB.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		ACT_Arduino_Helper_LIB.cpp
+ Created:	11/4/2022 4:42:43 AM
+ Author:	MarkAlicz
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "ACT_Arduino_Helper_LIB.h"
+
+
---
+++ ACT_Arduino_Libraries/ACT_Arduino_Helper_LIB/src/ACT_Arduino_Helper_LIB.h
@@ -0,0 +1,19 @@
+/*
+ Name:		ACT_Arduino_Helper_LIB.h
+ Created:	11/4/2022 4:42:43 AM
+ Author:	MarkAlicz
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _ACT_Arduino_Helper_LIB_h
+#define _ACT_Arduino_Helper_LIB_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
